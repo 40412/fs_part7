2b223c7106ecad599f20f4f364699aec90cbbcb4
40412 <jasmin.franti@gmail.com>
2026-06-08 22:25:45 +0300

    7.10

--- bloglist/bloglist-frontend/eslint.config.js
+++ bloglist/bloglist-frontend/eslint.config.js
@@ -1,12 +1,12 @@
-import js from "@eslint/js";
-import globals from "globals";
-import reactHooks from "eslint-plugin-react-hooks";
-import reactRefresh from "eslint-plugin-react-refresh";
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
-  { ignores: ["dist"] },
+  { ignores: ['dist'] },
   {
-    files: ["**/*.{test,spec}.{js,jsx}"],
+    files: ['**/*.{test,spec}.{js,jsx}'],
     languageOptions: {
       globals: {
         ...globals.browser,
@@ -15,28 +15,34 @@ export default [
     },
   },
   {
-    files: ["**/*.{js,jsx}"],
+    files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
       parserOptions: {
-        ecmaVersion: "latest",
+        ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
-        sourceType: "module",
+        sourceType: 'module',
       },
     },
     plugins: {
-      "react-hooks": reactHooks,
-      "react-refresh": reactRefresh,
+      'react-hooks': reactHooks,
+      'react-refresh': reactRefresh,
     },
     rules: {
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
-      "react-hooks/exhaustive-deps": "off",
-      "no-unused-vars": ["error", { varsIgnorePattern: "^[A-Z_]" }],
-      "react-refresh/only-export-components": "off",
-      quotes: ["error", "double"],
-      "arrow-spacing": ["error", { before: true, after: true }],
+      'react-hooks/exhaustive-deps': 'off',
+      'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react-refresh/only-export-components': 'off',
+      quotes: ['error', 'double'],
+      'arrow-spacing': ['error', { before: true, after: true }],
     },
+    extends: [
+      'eslint:recommended',
+      'plugin:react/recommended',
+      'plugin:react-hooks/recommended',
+      'plugin:prettier/recommended',
+    ],
   },
-];
+]
